3ef2b2e39686d12d05e49a6ffa3f98260dcab7de
Виталий Токтаров <96166@tyazhmash.com>
2024-04-12 07:50:17 +0400

    Домашнее задание 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { ListItemComponent } from './components/to-do-list/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoListComponent
+    ToDoListComponent,
+    ListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.scss']
+  styleUrls: ['./to-do-list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class ToDoListComponent {
 
+export class ToDoListComponent {
+  text: string = '';
+
+  itemList: {id: number, text: string}[] = [
+    {id: 0, text: 'Buy a new gaming laptop'},
+    {id: 1, text: 'Complete previous task'},
+    {id: 2, text: 'Create some angular app'}
+  ];
+
+  addItem(addText: string) {
+    this.itemList.push({
+      id: Math.max(...this.itemList.map(it => it.id)) + 1,
+      text: addText
+    });
+  }
 }
