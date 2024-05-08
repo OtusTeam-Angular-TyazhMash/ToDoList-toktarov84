efefc97f7c9826179f3ae8f8c654d017fcdd54a1
Виталий Токтаров <96166@tyazhmash.com>
2024-05-08 14:47:23 +0400

    Shared button через ng-content. Loading spinner в отдельный компонент в shared модуль.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,22 +4,29 @@ import { ButtonComponent } from './button/button.component';
 import { TooltipComponent } from './tooltip/tooltip.component';
 import { TooltipDirective } from './tooltip/tooltip.directive';
 import { ToastsComponent } from './toasts/toasts.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     TooltipComponent,
     TooltipDirective,
-    ToastsComponent
+    ToastsComponent,
+    SpinnerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     ButtonComponent,
     TooltipComponent,
     TooltipDirective,
-    ToastsComponent
+    ToastsComponent,
+    SpinnerComponent
   ]
 })
 export class SharedModule { }
